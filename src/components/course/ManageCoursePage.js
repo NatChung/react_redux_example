6a328ecdeb22a6e109cb684f3e1468a35b6393cd
Nat <nat@starvedia.com>
2017-11-05 09:09:56 +0800

    [A] Added course page

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -12,89 +12,26 @@ export class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, props.course),
-      errors: {},
-      saving: false
+      errors: {}
     };
-
-    this.updateCourseState = this.updateCourseState.bind(this);
-    this.saveCourse = this.saveCourse.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.course.id != nextProps.course.id) {
-      // Necessary to populate form when existing course is loaded directly.
-      this.setState({course: Object.assign({}, nextProps.course)});
-    }
-  }
-
-  updateCourseState(event) {
-    const field = event.target.name;
-    let course = Object.assign({}, this.state.course);
-    course[field] = event.target.value;
-    return this.setState({course: course});
-  }
-
-  courseFormIsValid() {
-    let formIsValid = true;
-    let errors = {};
-
-    if (this.state.course.title.length < 5) {
-      errors.title = 'Title must be at least 5 characters.';
-      formIsValid = false;
-    }
-
-    this.setState({errors: errors});
-    return formIsValid;
-  }
-
-
-  saveCourse(event) {
-    event.preventDefault();
-
-    if (!this.courseFormIsValid()) {
-      return;
-    }
-
-    this.setState({saving: true});
-
-    this.props.actions.saveCourse(this.state.course)
-      .then(() => this.redirect())
-      .catch(error => {
-        toastr.error(error);
-        this.setState({saving: false});
-      });
-  }
-
-  redirect() {
-    this.setState({saving: false});
-    toastr.success('Course saved');
-    this.context.router.push('/courses');
   }
 
   render() {
     return (
       <CourseForm
-        allAuthors={this.props.authors}
+        allAuthors={[]}
         onChange={this.updateCourseState}
-        onSave={this.saveCourse}
         course={this.state.course}
         errors={this.state.errors}
-        saving={this.state.saving}
       />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-  course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  course: PropTypes.object.isRequired
 };
 
-//Pull in the React Router context so router is available on this.context.router.
-ManageCoursePage.contextTypes = {
-  router: PropTypes.object
-};
 
 function getCourseById(courses, id) {
   const course = courses.filter(course => course.id == id);
@@ -103,17 +40,9 @@ function getCourseById(courses, id) {
 }
 
 function mapStateToProps(state, ownProps) {
-  const courseId = ownProps.params.id; // from the path `/course/:id`
-
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
-
-  if (courseId && state.courses.length > 0) {
-    course = getCourseById(state.courses, courseId);
-  }
-
   return {
-    course: course,
-    authors: authorsFormattedForDropdown(state.authors)
+    course: course
   };
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage'
+import ManagerCoursesPage from './components/course/ManageCoursePage'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManagerCoursesPage} />
+    <Route path="course/:id" component={ManagerCoursesPage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
