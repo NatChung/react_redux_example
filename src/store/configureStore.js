8181edcf489a20dcc8524e2b2eceadcf5639ce2d
Nat <nat@starvedia.com>
2017-11-05 08:33:53 +0800

    [A] Added thunk for loading courses

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux'
 import rootReucer from '../reducers'
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+import thunk from 'redux-thunk'
 
 export default function configureStore(initalState){
     return createStore(
         rootReucer,
         initalState,
-        applyMiddleware(reduxImmutableStateInvariant())
+        applyMiddleware(thunk, reduxImmutableStateInvariant())
     )
 }
